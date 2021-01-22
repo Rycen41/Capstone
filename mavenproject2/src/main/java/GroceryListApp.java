e801a286c8a0cef2923bd3c54c92ee8c57d2359e
Rycen41 <77689655+Rycen41@users.noreply.github.com>
2021-01-22 09:02:48 -0700

    finially

--- mavenproject2/src/main/java/GroceryListApp.java
+++ mavenproject2/src/main/java/GroceryListApp.java
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package GroceryListApp;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -10,4 +13,22 @@
  */
 public class GroceryListApp {
     
+    
+    
+    public static void main(String[] args) {
+        ShoppingCart cart = new ShoppingCart();
+        Store store = new Store();
+        
+        displayStoreMenu(store.getProducts());
+    }
+    
+    public static void displayStoreMenu(ArrayList<Product> products)
+    {
+        System.out.println("Items");
+        System.out.println("----------");
+        for(Product prod: products)
+        {
+            System.out.println(prod.getId() + ": " + prod.getName() + " - $" +  prod.getPrice());
+        }
+    }
 }
---
+++ mavenproject2/src/main/java/GroceryListApp/ShoppingCart.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroceryListApp;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Rycen
+ */
+public class ShoppingCart {
+    private ArrayList<ShoppingCartItem> items;
+    
+    public ShoppingCart()
+    {
+        items = new ArrayList<>();
+    }
+    
+    public double GetTotalPrice() {
+        
+        double total = 0;
+        for (ShoppingCartItem item: items)
+        {
+            total += item.GetTotalPrice();
+        }
+        return total;
+    }
+    
+    public boolean ItemExists(Product prod) {
+        
+        for(ShoppingCartItem item: items)
+        {
+            if (prod.getId() == item.GetProduct().getId())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public ShoppingCartItem GetExistingItem(Product prod) {
+        
+        for(ShoppingCartItem item: items)
+        {
+            if(prod.getId() == item.GetProduct().getId())
+                return item;
+        }
+        return null;
+    }
+    
+    public void addItem(Product prod, int quantity)
+    {
+        if (ItemExists(prod))
+        {
+            ShoppingCartItem item = GetExistingItem(prod);
+            item.setQuantity(item.GetQuantity() + quantity);
+        }
+        else {
+            items.add(new ShoppingCartItem(prod, quantity));
+        }
+    }
+    
+    public void removeItem(Product prod, int quantity)
+    {
+        if (ItemExists(prod))
+        {
+            ShoppingCartItem item = GetExistingItem(prod);
+            if(item.GetQuantity() > quantity)
+                item.setQuantity(item.GetQuantity()- quantity);
+            else
+                items.remove(item);
+        }
+    }
+    
+}
